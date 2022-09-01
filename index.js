fd2ddc32db090acfa9ca26edc8a70674ce0d3555
Muthiira <calebmuchiri48@gmail.com>
2022-09-02 02:25:32 +0300

    fetch_function

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
 const imgLink = 'http://localhost:3000/images/1'
 
 // fetch function
-
+function getImage() {
+    fetch(imgLink)
+        .then(resp => (resp).json())
+        .then(images => {
+            console.log(images)
+            document.getElementById('card-image').src = images.image;
+			document.getElementById('card-image1').src = images.image1;
+			document.getElementById('card-image2').src = images.image2;
+			document.getElementById('card-image3').src = images.image3;
+			document.getElementById('card-image4').src = images.image4;
+            document.getElementById('card-title').innerHTML = images.title;
+            document.getElementById('like-count').innerHTML = images.likes;
+            document.getElementById('Fact-list').innerHTML = images.comments.map(comments => `<li>${comments.facts}</li>`).join('');
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
